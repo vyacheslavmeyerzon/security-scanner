00479cf7c0bbdc5f102a3925dc71e458752646d9
Vyacheslav <vyacheslav.meyerzon@gmail.com>
2025-08-01 22:43:25 +0300

    ver 0.1.2

--- src/security_scanner/__init__.py
+++ src/security_scanner/__init__.py
@@ -5,7 +5,7 @@ This package provides functionality to scan Git repositories for exposed secrets
 API keys, passwords, and other sensitive information.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Vyacheslav Meyerzon"
 __email__ = "vyacheslav.meyerzon@gmail.com"
 
